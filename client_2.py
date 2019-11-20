d12aac449b764bc803d235cb5fdb212208b6f676
guibronzatti <guilherme.bronzatti@hotmail.com>
2019-11-20 19:31:19 -0300

    Update client_2.py

--- client_2.py
+++ client_2.py
@@ -3,31 +3,53 @@ from socket import AF_PACKET, SOCK_RAW
 from struct import *
 
 def unpack_ipv4(data):
-    ihl_version, tos, tot_len, id_ipv4, frag_off, ttl, protocol, check = unpack('!BBHHHBBH',data[:12]) #pacote de 20 bytes = 160 bits
-    #header = "{:32b}".format(bits_packet_1) ##para poder manipular bit a bit
-    #ihl_version = header[0:8]
-    #tos = header[8:16]
-    #tot_len = header[16:32]
-    #id_ipv4 = header[32:48]
-    #flags = header[48:51] ##nao usamos no pacote, mas caso precise ja esta aqui
-    #frag_off = header[51:64] 
-    #ttl = header[64:72]
-    #protocol = header[72:80]
-    #check = header[80:96]
-    #saddr = header[96:128]
-    #daddr = header[128:160]
-    return ihl_version, tos, tot_len, id_ipv4, frag_off, ttl, protocol, check
+    ihl_version, tos, tot_len, id_ipv4, frag_off, ttl, protocol, check, source_ip, dest_ip = unpack('!BBHHHBBH4s4s',data[:12]) #pacote de 20 bytes = 160 bits
+    return ihl_version, tos, tot_len, id_ipv4, frag_off, ttl, protocol, check, source_ip, dest_ip
 def unpack_udp(data): 
     src_port, dest_port, size, checksum = unpack('!HHHH', data[:8]) #Pacote de 8 bytes = 64 bits
-    #header = "{:8b}".format(bits_packet_1) ##como no ipv4, manipular bit a bit
-    #src_port = header[0:16]
-    #dest_port = header[16:32]
-    #size = header[32:48]
-    #checksum = header[48:64]
     return src_port, dest_port, size, checksum
+
+def sendeth(eth_frame, interface = "enp4s0"):
+	"""Send raw Ethernet packet on interface."""
+	s = socket.socket(AF_PACKET, SOCK_RAW)
+	s.bind((interface, 0))
+	return s.send(eth_frame)
+
+def checksum(msg):
+	s = 0
+	# loop taking 2 characters at a time
+	for i in range(0, len(msg), 2):
+		w = (msg[i] << 8) + (msg[i+1])
+		s = s + w
+
+	s = (s >> 16) + (s & 0xffff);
+	s = ~s & 0xffff
+
+	return s
+
+def get_mac_addr(bytes_addr):
+        bytes_str = map('{:02x}'.format, bytes_addr)
+        mac_addr = ':'.join(bytes_str).upper()
+        return mac_addr
+
+def unpack_eth_header(data):
+        dst_mac, src_mac, proto = unpack('!6s6sH', data)
+        return get_mac_addr(dst_mac),get_mac_addr(src_mac), socket.htons(proto), data[:14]
+
 
 if __name__ == "__main__":
 	
+    dst_mac = [0x00, 0x0a, 0x11, 0x11, 0x22, 0x22]
+	src_mac = [0xFF, 0xFF, 0xFF, 0x11, 0x22, 0x22]
+
+    eth_header = pack('!6B6BH', dst_mac[0], dst_mac[1], dst_mac[2], dst_mac[3], dst_mac[4], dst_mac[5], 
+	src_mac[0], src_mac[1], src_mac[2], src_mac[3], src_mac[4], src_mac[5], 0x0800)
+    
+    source_ip = '192.168.1.101'
+	dest_ip = '192.168.1.1'
+    received_packets = 0
+    state = 0
+
 	src_port = 1234
 	dst_port = 5678
 	data_length = 520
@@ -47,8 +69,17 @@ if __name__ == "__main__":
 	ttl = 255
 	protocol = 2
 	check = 0
-	
-	ip_header = pack('!BBHHHBBH' , ihl_version, tos, tot_len, id_j, frag_off, ttl, protocol, check)
+    source_ip = socket.inet_aton(source_ip)
+	dest_ip = socket.inet_aton(dest_ip)
+
+	ip_header = pack('!BBHHHBBH4s4s' , ihl_version, tos, tot_len, id_j, frag_off, ttl, protocol, check, source_ip, dest_ip)
 	print(len(ip_header))
 	print(unpack_ipv4(ip_header))
 
+    while(1){
+        if(state == 0):
+            print("Requesting to sever")
+            send_request()
+            state = 1
+        if(state == 1):
+    }
